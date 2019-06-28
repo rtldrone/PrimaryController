74a08180cd0d8e49c6f2cc6837a12ea67bb10cba
Cameron <camearle20@me.com>
2019-06-28 16:53:10 -0400

    Switched to printf for websocket responses, removed unnecessary mutex

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,32 @@
 #include <SPIFFS.h>
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
-#include <mutex>
 
 #define WS_WATCHDOG_TIMEOUT 2000
+
+enum BatteryState {
+  OK,
+  WARNING,
+  BAD
+};
 
 const IPAddress apIP(192, 168, 4, 1);
 const IPAddress subnet(255, 255, 255, 0);
 const char *ssid = "rtldrone";
+const char *oiResponseFmt = "S%.2f%i%.2f%.2fF%s"; //S[battery voltage][battery state][speed][speed setpoint]F[faults]
+
+//Live data
+double batteryVoltage = 12.00;
+int batteryState = BatteryState::OK;
+double currentSpeedMph = 5.00;
+double speedSetpointMph = 10.00;
 
 AsyncWebServer webserver(80);
 AsyncWebSocket websocket("/ws");
 
-std::mutex ws_recv_lock;
 unsigned long ws_last_recvd = 0;
 
 void onWsEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
-  ws_recv_lock.lock();
   if (type == WS_EVT_CONNECT) {
     Serial.println("WS Client Connected");
   }
@@ -28,10 +38,9 @@ void onWsEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventTyp
       //Update command, log it
       Serial.println("Got Update");
       //Respond
-      client->text(String(ws_last_recvd));
+      client->printf(oiResponseFmt, batteryVoltage, batteryState, currentSpeedMph, speedSetpointMph, "");
     }
   }
-  ws_recv_lock.unlock();
 }
 
 void setup() {
@@ -47,12 +56,10 @@ void setup() {
 }
 
 void loop() {
-  ws_recv_lock.lock();
   unsigned long current_time = millis();
   unsigned long dt = current_time - ws_last_recvd;
   if (dt > WS_WATCHDOG_TIMEOUT) {
     Serial.println("WS TIMEOUT");
   }
-  ws_recv_lock.unlock();
   delay(10);
 }
